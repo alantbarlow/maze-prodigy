118ec71133459df6b5689a984d95675781e4ea61
alantbarlow <alantbarlow@gmail.com>
2023-07-11 01:20:49 +0000

    Fix theme path

--- maze_prodigy/assets/themes/__init__.py
+++ maze_prodigy/assets/themes/__init__.py
@@ -4,8 +4,8 @@ import sys
 
 def get_theme_path(file_name: str):
 
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
+    if getattr(sys, 'frozen', False):
+        base_path = path.join(sys._MEIPASS, "themes")
+    else:
         base_path = path.abspath(path.dirname(__file__))
     return path.join(base_path, file_name)
