61e7bb6458ee980cd748a2484d3c83c72fc278d8
alantbarlow <alantbarlow@gmail.com>
2023-07-11 00:16:14 +0000

    Simplified the calculation of the theme path

--- maze_prodigy/assets/themes/__init__.py
+++ maze_prodigy/assets/themes/__init__.py
@@ -1,12 +1,11 @@
 from os import path
 import sys
-
+    
 
 def get_theme_path(file_name: str):
 
-    if getattr(sys, 'frozen', False):
-        bundle_dir = sys._MEIPASS
-        return path.join(bundle_dir, file_name)
-    else:
-        current_path = path.abspath(path.dirname(__file__))
-        return path.join(current_path, file_name)
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = path.abspath(path.dirname(__file__))
+    return path.join(base_path, file_name)
