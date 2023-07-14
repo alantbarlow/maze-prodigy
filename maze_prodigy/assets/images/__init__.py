1fba5e9e6f7a3cb8765739162caf859b70ffba4a
alantbarlow <alantbarlow@gmail.com>
2023-07-14 20:08:58 +0000

    update code to support the image file path

--- maze_prodigy/assets/images/__init__.py
+++ maze_prodigy/assets/images/__init__.py
@@ -1,6 +1,6 @@
-from pkgutil import get_data
 from typing import TYPE_CHECKING
-from io import BytesIO
+from os import path
+import sys
 
 from customtkinter import CTkImage
 from PIL import Image, ImageTk
@@ -10,12 +10,15 @@ if TYPE_CHECKING:
 
 
 def get_image(file_name: str, dimensions: "Dimensions", canvas_mode: bool = False ) -> CTkImage | ImageTk.PhotoImage:
-    image_data = get_data(__name__, file_name)
-    image = Image.open(BytesIO(image_data))
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = path.abspath(path.dirname(__file__))
+
+    image_path = path.join(base_path, "images", file_name)
+    image = Image.open(image_path)
     
     if canvas_mode:
         return ImageTk.PhotoImage(image.resize((dimensions.width, dimensions.height)))
-
     else:
         return CTkImage(dark_image = image, size = (dimensions.width, dimensions.height))
-    
