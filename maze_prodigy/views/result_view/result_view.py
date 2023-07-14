e2268f1bc198d1a35961b586f6d5b59bc35d4003
alantbarlow <alantbarlow@gmail.com>
2023-07-14 20:10:27 +0000

    Remove popup window and no longer used code

--- maze_prodigy/views/result_view/result_view.py
+++ maze_prodigy/views/result_view/result_view.py
@@ -1,8 +1,6 @@
-from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
-from customtkinter import CTkFrame, CTkButton, CTkToplevel, CTkLabel
-from PIL import Image, ImageTk
+from customtkinter import CTkFrame, CTkButton
 
 from ..view import View
 from .progress_frame import ProgressFrame
@@ -25,17 +23,8 @@ class ResultView(View):
             main_menu_button_command: "Callable[..., None]"
         ):
         super().__init__()
-
-        popup_window = CTkToplevel(master = window)
-        popup_window.attributes('-fullscreen', True)
-        popup_window.geometry("{}x{}".format(
-            window.winfo_width(), 
-            window.winfo_height(), 
-        ))
-        popup_window.wait_visibility()
-        popup_window.attributes('-alpha', 0.6)
         
-        frame = CTkFrame(popup_window, fg_color = "grey14")
+        frame = CTkFrame(window, fg_color = "grey14")
         frame.pack(expand = True)
 
         StarFrame(frame, move_percentage, time_percentage).pack(pady = 50)
@@ -44,11 +33,11 @@ class ResultView(View):
         ProgressFrame(frame, "Accuracy", move_percentage).pack(pady = 20, padx = 50)
         CTkButton(frame, text = "Back to Main Menu", command = main_menu_button_command).pack(pady = 50)
 
-        self.__popup_window = popup_window
+        self.__root_frame = frame
  
 
     def destroy(self):
-        self.__popup_window.destroy()
+        self.__root_frame.destroy()
 
     def get_state(self):
         raise NotImplementedError
